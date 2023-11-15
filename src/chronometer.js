679089a41f06cba9875013a49238a1d11ff9272d
Andeliz Flores <andelizflores@outlook.com>
2023-11-15 17:17:59 -0400

    Chronometers v1

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    let intervalId = setInterval(() => {
+    this.currentTime += 1
+    if(printTimeCallback){
+      printTimeCallback()
+    }
+    }, 1000)
+    this.intervalId = intervalId;
+}
+
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+     return (minutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime % 60);
+     return (seconds);
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  computeTwoDigitNumber(value){
+    let i = '0' + value;
+    return i.slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutes}:${seconds}`;
   }
 }
